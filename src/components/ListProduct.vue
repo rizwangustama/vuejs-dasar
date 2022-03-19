9cc81bebd058bec28d2d1779f83241675279dd23
rizwangustama <rizonegustama@gmail.com>
2022-03-19 22:39:02 +0700

    fech API product

--- src/components/ListProduct.vue
+++ src/components/ListProduct.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="Product" class="container">
-    <header class="sticky top-0 py-5 lg:w-2/4">
+    <!-- <header class="sticky top-0 py-5 lg:w-2/4">
       <div class="form flex gap-10 items-center">
         <label class="w-1/4" for="">Maxsimal Price</label>
         <input
@@ -23,25 +23,43 @@
         v-model="maksimal"
         max="1000"
       />
-    </header>
+    </header> -->
     <div class="grid lg:grid-cols-3 gap-6">
       <div v-for="(listProduct, index) in product" :key="index">
         <div
-          class="card p-5 rounded-3xl overflow-hidden shadow-xl"
+          class="card p-5 rounded-3xl overflow-hidden shadow-xl h-full"
           v-if="listProduct.price <= Number(maksimal)"
         >
           <img
-            class="mx-auto rounded-2xl w-full"
-            :src="listProduct.images"
+            class="mx-auto rounded-2xl w-full w-full object-fit object-cover"
+            style="height: 400px"
+            height="400px"
+            :src="listProduct.image"
             :alt="listProduct.name"
           />
           <div class="content py-4">
             <h1 class="text-2xl font-semibold">{{ listProduct.name }}</h1>
-            <p class="text-xl font-semibold">
-              {{ listProduct.price }}
-            </p>
-            <div class="h3">Rp {{ listProduct.price }}</div>
-            <p>No.{{ index + 1 }}</p>
+            <p>{{ listProduct.description }}</p>
+            <div class="flex mt-3 justify-between">
+              <p class="py-1 px-4 text-white bg-slate-500 rounded-full">
+                No.{{ index + 1 }}
+              </p>
+              <p
+                class="
+                  text-xl
+                  font-semibold
+                  py-1
+                  px-10
+                  bg-green-300
+                  flex
+                  rounded-full
+                  justify-start
+                  text-right
+                "
+              >
+                $ {{ listProduct.price }}
+              </p>
+            </div>
           </div>
           <button
             class="py-2 px-5 bg-black rounded-full text-white w-full"
@@ -62,34 +80,15 @@ export default {
     return {
       maksimal: 1000,
       tambah: null,
-      product: [
-        {
-          name: "Product 1",
-          price: "100",
-          images: "https://unsplash.it/400/300",
-        },
-        {
-          name: "Product 2",
-          price: "200",
-          images: "https://unsplash.it/400/300",
-        },
-        {
-          name: "Product 3",
-          price: "300",
-          images: "https://unsplash.it/400/300",
-        },
-        {
-          name: "Product 4",
-          price: "400",
-          images: "https://unsplash.it/400/300",
-        },
-        {
-          name: "Product 5",
-          price: "500",
-          images: "https://unsplash.it/400/300",
-        },
-      ],
+      product: null,
     };
+  },
+  mounted: function () {
+    fetch("https://hplussport.com/api/products/order/price")
+      .then((response) => response.json())
+      .then((data) => {
+        this.product = data;
+      });
   },
 };
 </script>
